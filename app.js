67bc4b7b36f456e80db5393b5d543ad2a8f45ac0
Shannon Mettry <shannonmettry12@gmail.com>
2023-06-20 12:29:44 +0200

    added sublinks to navbar and added javascript lazy loading to help loading speed of website

---
+++ app.js
@@ -0,0 +1,22 @@
+// JavaScript code for lazy loading
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+  
+    if ("IntersectionObserver" in window) {
+      var lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            var lazyImage = entry.target;
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.classList.remove("lazy");
+            lazyImageObserver.unobserve(lazyImage);
+          }
+        });
+      });
+  
+      lazyImages.forEach(function(lazyImage) {
+        lazyImageObserver.observe(lazyImage);
+      });
+    }
+  });
+  
